9d7a9d50939d5ff1ff8c15625941143b84f641f6
passtika0826@gmail.com <agus@Widya-MacBook-Pro.local>
2023-12-14 01:10:32 +0800

    firs commit reaachs

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from '../src/components/Header'
+
+function App() {
+  return (
+    <>  
+     <Header/>
+     <h1>Pastika belajar reacjs</h1>
+    </>
+  );
+}
+
+export default App;
